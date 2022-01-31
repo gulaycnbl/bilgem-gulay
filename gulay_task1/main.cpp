6e6be975c36681ed76302cb8c218a8d5c5131d9a
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-01-31 10:55:27 +0300

    clear unused parts

--- gulay_task1/main.cpp
+++ gulay_task1/main.cpp
@@ -8,33 +8,8 @@
 #define PRG3 "jfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfgıjisrejgesiı rjgıofvdşhsrb podgjudpfıohjb gtdtxhb hukhgkdfhkgxdhn hjodghıdlxş gfhıoufh"
 #define PRG4 "fugvdhnd kfvnfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfg hıhuı hukhgkdfh bkbdn ngvffdıogn gvdbkbdn ngvffdıogn ngvkjxgbdkf rfghgkdfh bkbdn ngvffdıogn ngvkjhıhuı hukhgkdfh bkbjfbsı"
 
-#define FILEPATH "./lorem2.txt"
+#define FILEPATH "/home/admin/Belgeler/GitHub/bilgem-gulay/gulay_task1/lorem.txt"
 #define MAXLEN 350
-#define NUMOFLINES 5
-
-char **lines_to_check = (char **)malloc(NUMOFLINES*sizeof(char*));
-/*const char * myarray[]{
-    PRG1,
-    PRG2,
-    PRG3,
-    PRG4,
-};
-*/ //can also be used as lines_to_check
-
-void assignLines(){
-    for(int i=0; i<NUMOFLINES; i++){
-        lines_to_check[i] = (char *)malloc(MAXLEN*sizeof(char));
-    }
-    lines_to_check[0] = PRG1;
-    lines_to_check[1] = PRG2;
-    lines_to_check[2] = PRG3;
-    lines_to_check[3] = PRG4;
-}
-
-void freeLines(char **lines){
-    for(int i=0; i<NUMOFLINES; i++) free(lines[i]);
-    free(lines);
-}
 
 int main() {
     char buffer[MAXLEN];
@@ -45,24 +20,18 @@ int main() {
         return(-1);
     }
 
-    assignLines();
-
     int count = 0;
-    int count_for_assert = 0;
 
     while(fgets(buffer, sizeof(buffer), fp) != nullptr){
         count++;
         if(buffer[0] == '#' || buffer[0] == '\n'){
             continue;
         } else{
-            //assert(strcmp(lines_to_check[count_for_assert], buffer));
-            count_for_assert++;
             printf("Paragraph in line %d: ", count);
             puts(buffer);
         }
     }
 
-    freeLines(lines_to_check);
     fclose(fp);
     return 0;
 }
