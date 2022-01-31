b3f9af2ecf1e99a123a0a4381840513c3329538a
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-01-31 09:48:29 +0300

    gulay_task1 added
    Test if the file "lorem.txt" is read correctly.

---
+++ gulay_task1/main.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <cassert>
+#include <stdlib.h>
+#include <cstring>
+
+#define PRG1 "bchfvs dfcbhsdjfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfg hı huı hukhgkdfhkgxdhn hjodghıdlxş gfhıoufhgodgtrh ghbr"
+#define PRG2 "djfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfg hıhuı hukhgkdfh bkbdn ngvffdıogn ngvkjhıhuı hukhgkdfh bkbjfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvk"
+#define PRG3 "jfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjfbsı bcdfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfgıjisrejgesiı rjgıofvdşhsrb podgjudpfıohjb gtdtxhb hukhgkdfhkgxdhn hjodghıdlxş gfhıoufh"
+#define PRG4 "fugvdhnd kfvnfızvbdı gvdbkbdn ngvffdıogn ngvkjxgbdkf rfg hıhuı hukhgkdfh bkbdn ngvffdıogn gvdbkbdn ngvffdıogn ngvkjxgbdkf rfghgkdfh bkbdn ngvffdıogn ngvkjhıhuı hukhgkdfh bkbjfbsı"
+
+#define FILEPATH "/home/admin/Genel/CLionProjects/gulay_task1/lorem.txt"
+#define MAXLEN 350
+#define NUMOFLINES 5
+
+char **lines_to_check = (char **)malloc(NUMOFLINES*sizeof(char*));
+const char * myarray[]{
+    PRG1,
+    PRG2,
+    PRG3,
+    PRG4,
+};
+
+void assignLines(){
+    /*for(int i=0; i<NUMOFLINES; i++){
+        lines_to_check[i] = (char *)malloc(MAXLEN*sizeof(char));
+    }*/
+    lines_to_check[0] = PRG1;
+    lines_to_check[1] = PRG2;
+    lines_to_check[2] = PRG3;
+    lines_to_check[3] = PRG4;
+}
+
+void freeLines(char **lines){
+    for(int i=0; i<NUMOFLINES; i++) free(lines[i]);
+    free(lines);
+}
+
+int main() {
+    char buffer[MAXLEN];
+    FILE *fp;
+    fp = fopen(FILEPATH, "r");
+    if(fp == nullptr){
+        perror("Cannot open file!");
+        return(-1);
+    }
+
+    assignLines();
+
+    int count = 0;
+    int count_for_assert = 0;
+
+    while(fgets(buffer, sizeof(buffer), fp) != nullptr){
+        count++;
+        if(buffer[0] == '#' || buffer[0] == '\n'){
+            continue;
+        } else{
+            assert(strcmp(myarray[count_for_assert], buffer));
+            count_for_assert++;
+            printf("Paragraph in line %d: ", count);
+            puts(buffer);
+        }
+    }
+
+    freeLines(lines_to_check);
+    fclose(fp);
+    return 0;
+}
