8ceeb6e2bdc39dc19aeefb7c73a5a303a2cde2b3
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-02-01 10:42:33 +0300

    #1 Update task1

---
+++ gulay_task1/cmake-build-debug/CMakeFiles/gulay_task1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gulay_task1.dir/main.cpp.o"
+  "gulay_task1"
+  "gulay_task1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gulay_task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
