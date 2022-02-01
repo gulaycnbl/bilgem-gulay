5cf64ff1c88de558096c217eb7303f32b62488c9
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-02-01 16:52:51 +0300

    Add function readAndAssertWithIndex()

--- gulay-task2/main.cpp
+++ gulay-task2/main.cpp
@@ -11,7 +11,7 @@
 //#define PRG6 "Tyger Tyger burning bright, In the forests of the night: What immortal hand or eye,Dare frame thy fearful symmetry?"
 
 #define FILENAME "newFile.txt"
-#define MAXLEN 350
+//#define MAXLEN 350
 
 char prg1[] = "Tyger Tyger, burning bright, In the forests of the night; What immortal hand or eye, Could frame thy fearful symmetry?";
 char prg2[] = "In what distant deeps or skies. Burnt the fire of thine eyes?On what wings dare he aspire?What the hand, dare seize the fire?";
@@ -21,7 +21,7 @@ char prg4[] = "What the hammer? what the chain, In what furnace was thy brain?Wh
 //int lineCount = 0;
 int lastIndex = 0;
 int maxIndex = 0;
-int lastStrLen = 0;
+unsigned long lastStrLen = 0;
 
 /*
 void writeToSpecificLine(FILE *fp, int line, char *str){
@@ -60,10 +60,24 @@ void customWriteToNewFile(){
     writeToSpecificIndex(fp, 0, prg1);
     writeToSpecificIndex(fp, 1000, prg2);
     writeToSpecificIndex(fp, 10000, prg3);
-    writeToSpecificIndex(fp, 300, prg4);
+    writeToSpecificIndex(fp, 1000000, prg4);
 
     //writeToSpecificLine(fp, 4, PRG4);
     fclose(fp);
+}
+
+void readAndAssertWithIndex(FILE *fp, char prg[], int index){
+    unsigned long lenPrg = strlen(prg);
+    char buffer[lenPrg+1];
+    char copybuf[lenPrg+1];
+    memset(copybuf, '0', lenPrg*sizeof(char));
+
+    fseek(fp, index, SEEK_SET);
+    fgets(buffer, sizeof(buffer), fp);
+    strncpy(copybuf, buffer, lenPrg+1);
+    //assert(strcmp(prg, copybuf));
+    printf("%s\n", copybuf);
+    printf("%s\n", prg);
 }
 
 void readTheParagraphs(){
@@ -73,54 +87,10 @@ void readTheParagraphs(){
         perror("Cannot open file to read!");
     }
 
-    char buffer[MAXLEN];
-    char copybuf[MAXLEN];
-    memset(copybuf, '0', MAXLEN*sizeof(char));
-    int lenPrg1 = strlen(prg1);
-    int lenPrg2 = strlen(prg2);
-    int lenPrg3 = sizeof(prg3)/sizeof(char);
-    int lenPrg4 = strlen(prg4);
-
-    /*fgets(buffer, sizeof(buffer), fp);
-    //printf("cursor: %ld\n", ftell(fp));
-    strncpy(copybuf, buffer, lenPrg1);
-    printf("%s", copybuf);
-    assert(strcmp(prg1, buffer));  //WHY THIS WORKS ONLY WITH BUFFER?
-    printf("\n");*/
-
-    fseek(fp, 0, SEEK_SET);
-    fgets(buffer, sizeof(buffer), fp);
-    strncpy(copybuf, buffer, lenPrg1);
-    printf("%s", copybuf);
-    assert(strcmp(prg1, buffer));
-    printf("\n");
-
-    fseek(fp, 1000, SEEK_SET);
-    fgets(buffer, sizeof(buffer), fp);
-    strncpy(copybuf, buffer, lenPrg2);
-    printf("%s", copybuf);
-    assert(strcmp(prg2, copybuf));
-    printf("\n");
-
-    /*fseek(fp, 10000, SEEK_SET);
-    fgets(buffer, sizeof(buffer), fp);
-    strncpy(copybuf, buffer, lenPrg3-1);
-    printf("%s", copybuf);
-    assert(strcmp(prg3, copybuf));
-    printf("\n");*/
-
-    fseek(fp, -lenPrg3+1, SEEK_END);
-    fgets(buffer, sizeof(buffer), fp);
-    assert(strcmp(prg3, copybuf));
-    printf("%s", buffer);
-    printf("\n");
-
-    fseek(fp, 300, SEEK_SET);
-    fgets(buffer, sizeof(buffer), fp);
-    strncpy(copybuf, buffer, lenPrg4);
-    printf("%s", copybuf);
-    assert(strcmp(prg4, copybuf));
-    printf("\n");
+    readAndAssertWithIndex(fp, prg1, 0);
+    readAndAssertWithIndex(fp, prg2, 1000);
+    readAndAssertWithIndex(fp, prg3, 10000);
+    readAndAssertWithIndex(fp, prg4, 1000000);
 
     fclose(fp);
 }
