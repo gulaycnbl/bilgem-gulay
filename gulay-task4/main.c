935077a1f27e03ce1316a1dfe3cebfb3a333c4ac
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-02-04 14:04:30 +0300

    Add mypow() to update read & write

--- gulay-task4/main.c
+++ gulay-task4/main.c
@@ -6,20 +6,36 @@
 #include <unistd.h>
 
 #define FILENAME "lorem.txt"
+#define NUMOFPRGS 6
 
-char prg1[] = "1-Tyger Tyger, burning bright, In the forests of the night; What immortal hand or eye, Could frame the fearful symmetry?";
-char prg2[] = "2-In what distant deeps or skies Burnt the fire of thine eyes? On what wings dare he aspire? What the hand, dare seize the fire?";
-char prg3[] = "3-And what shoulder, & what art, Could twist the sinews of the heart? And when the heart began to beat, What dread hand? & what dread feet?";
-char prg4[] = "4-What the hammer? what the chain, In what furnace was the brain? What the anvil? what dread grasp, Dare its deadly terrors clasp!";
-char prg5[] = "5-When the stars threw down their spears And water'd heaven with their tears, Did he smile his work to see? Did he who made the Lamb make thee?";
-char prg6[] = "6-Tyger Tyger burning bright, In the forests of the night, What immortal hand or eye,Dare frame the fearful symmetry?";
+char prg0[] = "1-Tyger Tyger, burning bright, In the forests of the night; What immortal hand or eye, Could frame the fearful symmetry?";
+char prg1[] = "2-In what distant deeps or skies Burnt the fire of thine eyes? On what wings dare he aspire? What the hand, dare seize the fire?";
+char prg2[] = "3-And what shoulder, & what art, Could twist the sinews of the heart? And when the heart began to beat, What dread hand? & what dread feet?";
+char prg3[] = "4-What the hammer? what the chain, In what furnace was the brain? What the anvil? what dread grasp, Dare its deadly terrors clasp!";
+char prg4[] = "5-When the stars threw down their spears And water'd heaven with their tears, Did he smile his work to see? Did he who made the Lamb make thee?";
+char prg5[] = "6-Tyger Tyger burning bright, In the forests of the night, What immortal hand or eye,Dare frame the fearful symmetry?";
+
+const char *arrayForPrgs[6] = {
+        prg0,
+        prg1,
+        prg2,
+        prg3,
+        prg4,
+        prg5,
+};
 
 long lastIndex = 0;
 long maxIndex = 0;
 unsigned long lastStrLen = 0;
+pthread_mutex_t lock;
+
+long my_pow(int a, long b){
+    if(b==0) return 1;
+    return a * my_pow(a, b - 1);
+}
 
 void write_dummy_string_from_n_character(FILE *fp, unsigned long n){
-    for(int i = 0; i < n; i++){
+    for(long i = 0; i < n; i++){
         char randomletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ    "[rand() % 30];
         fprintf(fp, "%c", randomletter);
     }
@@ -42,12 +58,15 @@ void custom_write_to_new_file(){
     if(fp == NULL){
         perror("Cannot open file to write!");
     }
-    write_to_file_index(fp, 0, prg1);
-    write_to_file_index(fp, 1000, prg2);
-    write_to_file_index(fp, 10000, prg3);
-    write_to_file_index(fp, 1000000, prg4);
-    write_to_file_index(fp, 100000000, prg5);
-    write_to_file_index(fp, 10000000000, prg6);
+    write_to_file_index(fp, 1, prg0);
+    for(int i=0; i<NUMOFPRGS; i++){
+        //index that violates the series (it's 1000 because 1 and 100 is too close)
+        if(i==1){
+            write_to_file_index(fp, 1000, arrayForPrgs[i]);
+        }else{
+            write_to_file_index(fp, my_pow(100, i), arrayForPrgs[i]);
+        }
+    }
 
     fclose(fp);
 }
@@ -62,31 +81,63 @@ void read_and_assert_with_index(FILE *fp, char prg[], long index){
     printf("The %lu indexed content of the file (%s) matches with the predefined content.\n", index, FILENAME);
 }
 
-void open_file(){
-    FILE *fp= fopen(FILENAME, "r+");
+void *thread_func(int *indArr[]){
+    /*pthread_once_t once_control = PTHREAD_ONCE_INIT;
+    pthread_once(&once_control, open_file);*/
+    /*FILE *fp= fopen(FILENAME, "r+");
     if(fp == NULL){
         perror("Cannot open file to read! You can either provide an existing file and change the FILENAME, "
                "or run the custom_write_to_new_file() function once");
+    }*/
+
+    /*
+   for(int i=0; i<3; i++){
+        pthread_mutex_lock(&lock);
+        read_and_assert_with_index(fp, array[i], indArr[i]);
+        pthread_mutex_unlock(&lock);
     }
+    */
+
+    //printf("%d\n",indArr[0]);
+
+    //printf("%s\n", array[0]);
+    //read_and_assert_with_index(fp, array[0], indArr[0]);
+    //read_and_assert_with_index(fp, arrayForAssertForThread2[0], indexArray2[0]);
+    return NULL;
 }
 
-void *thread_func(){
-    /*pthread_once_t once_control = PTHREAD_ONCE_INIT;
-    pthread_once(&once_control, open_file);*/
+int main() {
     FILE *fp= fopen(FILENAME, "r+");
     if(fp == NULL){
         perror("Cannot open file to read!");
     }
 
-    read_and_assert_with_index(fp, prg1, 0);
-}
+    for(int i=0; i<NUMOFPRGS; i++){
+        //index that violates the series (it's 1000 because 1 and 100 is too close)
+        if(i==1){
+            read_and_assert_with_index(fp, arrayForPrgs[i], 1000);
+        }else{
+            read_and_assert_with_index(fp, arrayForPrgs[i], my_pow(100, i));
+        }
+    }
+    /*
+    pthread_t tid[2];
 
-int main() {
-    pthread_t tid;
+    printf("MAİN, indexArr1[0]: %d\n", indexArray1[0]);
+    printf("MAİN, indexArr1[1]: %d\n", indexArray1[1]);
+    printf("MAİN, indexArr1[2]: %d\n", indexArray1[2]);
 
-    pthread_create(&tid, NULL, thread_func, NULL);
-    pthread_create(&tid, NULL, thread_func, NULL);
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
 
-    pthread_exit(NULL);
+    pthread_create(&(tid[0]), NULL, &thread_func, (void *)indexArray1);
+    //pthread_create(&(tid[1]), NULL, &thread_func, (arrayForAssertForThread2, indexArray2));
+
+    pthread_join(tid[0], NULL);
+    //pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&lock);*/
     return 0;
 }
