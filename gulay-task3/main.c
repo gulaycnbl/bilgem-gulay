55b42e95d38a867eb64225fe6136ab4b72035236
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-02-08 08:15:26 +0300

    #6 Change variable names

--- gulay-task3/main.c
+++ gulay-task3/main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define FILENAME "lorem.txt"
+#define FILE_NAME "lorem.txt"
 
 char prg1[] = "1-Tyger Tyger, burning bright, In the forests of the night; What immortal hand or eye, Could frame the fearful symmetry?";
 char prg2[] = "2-In what distant deeps or skies Burnt the fire of thine eyes? On what wings dare he aspire? What the hand, dare seize the fire?";
@@ -13,9 +13,9 @@ char prg4[] = "4-What the hammer? what the chain, In what furnace was the brain?
 char prg5[] = "5-When the stars threw down their spears And water'd heaven with their tears, Did he smile his work to see? Did he who made the Lamb make thee?";
 char prg6[] = "6-Tyger Tyger burning bright, In the forests of the night, What immortal hand or eye,Dare frame the fearful symmetry?";
 
-long lastIndex = 0;
-long maxIndex = 0;
-unsigned long lastStrLen = 0;
+long previous_index = 0;
+long max_index = 0;
+unsigned long last_str_len = 0;
 
 void write_dummy_string_from_n_character(FILE *fp, unsigned long n){
     for(int i = 0; i < n; i++){
@@ -25,19 +25,19 @@ void write_dummy_string_from_n_character(FILE *fp, unsigned long n){
 }
 
 void write_to_file_index(FILE *fp, long index, char *str){
-    if(index>maxIndex){
-        maxIndex = index;
-        unsigned long dummyStrLen = index-lastIndex-lastStrLen;
+    if(index > max_index){
+        max_index = index;
+        unsigned long dummyStrLen = index - previous_index - last_str_len;
         write_dummy_string_from_n_character(fp, dummyStrLen);
     }
-    lastStrLen = strlen(str);
-    lastIndex = index;
+    last_str_len = strlen(str);
+    previous_index = index;
     fseek(fp, index, SEEK_SET);
     fprintf(fp, "%s", str);
 }
 
 void custom_write_to_new_file(){
-    FILE *fp= fopen(FILENAME, "w+");
+    FILE *fp= fopen(FILE_NAME, "w+");
     if(fp == NULL){
         perror("Cannot open file to write!");
     }
@@ -58,12 +58,12 @@ void read_and_assert_with_index(FILE *fp, char prg[], long index){
     fseek(fp, index, SEEK_SET);
     fgets(buffer, sizeof(buffer), fp);
     assert(!strcmp(prg, buffer));
-    printf("The %lu indexed content of the file (%s) matches with the predefined content.\n", index, FILENAME);
+    printf("The %lu indexed content of the file (%s) matches with the predefined content.\n", index, FILE_NAME);
 }
 
 void execute_child_process(){
     FILE *fp;
-    fp = fopen(FILENAME, "r+");
+    fp = fopen(FILE_NAME, "r+");
     if(fp == NULL){
         perror("Cannot open file to read!");
     }
@@ -75,7 +75,7 @@ void execute_child_process(){
 
 void execute_parent_process(){
     FILE *fp;
-    fp = fopen(FILENAME, "r+");
+    fp = fopen(FILE_NAME, "r+");
     if(fp == NULL){
         perror("Cannot open file to read!");
     }
