e6a77d37c00166901feded03fdfb5f995d24e7fc
Gülay Canbaloglu <60276129+gulaycnbl@users.noreply.github.com>
2022-02-15 10:25:29 +0300

    Task 6 read with fscanf()

--- gulay-task6/main.c
+++ gulay-task6/main.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FILE_NAME "task100.config"
-#define MAX_LEN 300
+#define MAX_LEN 1500
 
-int retrieved_number;
-
-void get_number_from_first_line(FILE *fp){
-    char buffer[MAX_LEN];
+int get_number_from_first_line(FILE *fp){
+    char buffer[10];
     fgets(buffer, sizeof(buffer), fp);
-    retrieved_number = atoi(buffer);
-}
-
-int has_line_content(char buffer[]){
-    return (buffer[0] != '#' && buffer[0] != '\n');
+    return atoi(buffer);
 }
 
 void read_file(FILE *fp){
     char buffer[MAX_LEN];
     int count = 0;
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        if(has_line_content(buffer)){
-            printf("%s\n", buffer);
-            count++;
-        }
-    }
-    printf("ret_num: %d, count: %d", retrieved_number, count);
+    fscanf(fp, "%200s %[\n\n]");
+    //printf("ret_num: %d, count: %d", retrieved_number, count);
 }
 
 int main() {
@@ -34,8 +24,15 @@ int main() {
         perror("Cannot open file!");
         return(-1);
     }
-    get_number_from_first_line(fp);
-    read_file(fp);
+
+    int num = get_number_from_first_line(fp);
+    char line[MAX_LEN];
+    fgets(line, sizeof line, fp);
+    for(int i=0; i<num; i++){
+        fscanf(fp, "%[^\n]\n", &line);
+        printf("%s\n", line);
+    }
+
 
     fclose(fp);
     return 0;
